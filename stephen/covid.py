85c55604ee1f8bb7d646254638c2a73916724bbd
Stephen Brock <sbrock@uwaterloo.ca>
2021-01-22 13:11:59 -0800

    initial commit

---
+++ stephen/covid.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import plotly.express as px  # (version 4.7.0)
+import plotly.graph_objects as go
+
+import dash  # (version 1.12.0) pip install dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+
+app.layout = html.Div([
+
+    html.H1("COVID-19 Dashboard", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id="slct_year",
+                 options=[
+                     {"label": "2015", "value": 2015},
+                     {"label": "2016", "value": 2016},
+                     {"label": "2017", "value": 2017},
+                     {"label": "2018", "value": 2018}],
+                 multi=False,
+                 value=2015,
+                 style={'width': "40%"}
+                 ),
+
+    html.Div(id='output_container', children=[]),
+    html.Br(),
+
+    dcc.Graph(id='my_bee_map', figure={})
+
+])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
